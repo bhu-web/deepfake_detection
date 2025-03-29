eecf3e8adb205cd62f0b62a48250d9682e1887ec
Bhoomi Yadav <bhoomiyadav876@gmail.com>
2025-03-29 22:18:29 +0530

    fastapi working

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from backend.routes.image_video import router as image_video_router
-from backend.routes.audio_text import router as audio_text_router
 
 app = FastAPI()
 
 app.include_router(image_video_router, prefix="/image-video")
-app.include_router(audio_text_router, prefix="/audio-text")
 
 @app.get("/")
 def home():
